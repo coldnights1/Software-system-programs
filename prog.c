18cd246a8e3aa516d2e66cafaf24a00dd7c11113
Prajit Banerjee <prajit99@gmail.com>
2023-08-25 10:39:29 +0530

    first

---
+++ prog.c
@@ -0,0 +1,26 @@
+#include <sys/types.h> // Import for `open` system call
+#include <sys/stat.h>  // Import for `open` system call
+#include <fcntl.h>     // Import for `open` system call
+#include <unistd.h>    // Import for `write` call
+#include <stdio.h>     // Import for `printf` & `perror` function
+
+void main(int argc, char *argv[])
+{
+
+    char *filename;
+    int fileDescriptor; // FD -> Success, -1 -> Failure
+
+    if (argc != 2)
+        write(STDOUT_FILENO, "Pass the file to be opened as the argument\n", 43);
+    else
+    {
+        filename = argv[1];
+
+        fileDescriptor = open(filename, O_RDWR);
+
+        if (fileDescriptor == -1)
+            perror("Error while opening the file!");
+        else
+            printf("The file was successfully created with the file descriptor -> %dn", fileDescriptor);
+    }
+}
---
+++ prog1.c
@@ -0,0 +1,26 @@
+
+#include <sys/types.h> // Import for `creat` system call
+#include <sys/stat.h>  // Import for `creat` system call
+#include <fcntl.h>     // Import for `creat` system call
+#include <stdio.h>     // Import for `printf` & `perror` function
+
+void main(int argc, char *argv[])
+{
+
+    char *filename;
+    int fileDescriptor; // FD -> Success, -1 -> Error
+
+    if (argc != 2)
+        printf("Pass the file to open as argument\n");
+    else
+    {
+        filename = argv[1];
+
+        fileDescriptor = creat(filename, S_IRWXU);
+
+        if (fileDescriptor == -1)
+            perror("Error while creating the file!");
+        else
+            printf("The file was successfully created with the file descriptor -> %dn", fileDescriptor);
+    }
+}
